742197472e5e55a2a95f8c5a55155ba0dec1618b
Marta Kulig <bialka104b@interia.pl>
2020-10-28 20:07:42 +0100

    the Product.js, ForUser.js component was built

--- src/pages/ForUser.js
+++ src/pages/ForUser.js
@@ -1,9 +1,14 @@
-//Nasza podstrona O nas
+//Nasza podstrona dla użytkowników zalogowanych
+import {Route, Redirect} from 'react-router-dom';
 
+const permission = false;
 function ForUser() {
   return (
     <div className="for-user">
-      Komponent ForUser
+      <Route render={() => (
+        // jeśli użtkownik ma pozwolenie na naszą strone to wyrenderuj h1, jesli nie to wyrenderuj strone główną
+        permission ? (<h1>Tylko dla uzytkowników zalogowanych</h1>) : (<Redirect to="/"/>)
+      )}/>
     </div>
   );
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,9 +1,23 @@
 //Nasza podstrona Produktu
+import {Link} from 'react-router-dom';
 
 function Product() {
+  const productsArray = ['produkt 1', 'produkt 2', 'produkt 3'];
+  const list = productsArray.map((product, index) => {
+    return (
+      <li className="product__list-li" key={index}>
+        <Link to="/about">{product}</Link>
+      </li>
+    )
+  })
   return (
     <div className="product">
-      Komponent Product
+      <h2>Lista produktów: </h2>
+      <div>
+        <ul className="product__list">
+          {list}
+        </ul>
+      </div>
     </div>
   );
 }
